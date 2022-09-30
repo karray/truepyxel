b178fdae192a974a86c6ff8768256c5d960b7b07
Aray Karjauv <kararray@gmail.com>
2022-09-30 16:12:44 +0200

    fix typos, more status

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
+let status = document.getElementById('status');
 let msg = document.getElementById('msg');
+let output = document.getElementById('output');
 let inputFile = document.getElementById('file');
 
 let source = document.getElementById('source');
@@ -7,24 +9,31 @@ let result = document.getElementById('result');
 let windowSize = document.getElementById('window-size');
 let pixelSize = document.getElementById('pixel-size');
 
+function updateStatus(msg){
+    output.innerHTML=msg
+    console.log(msg)
+}
+
 async function main() {
     try {
-        window.pyodide = await loadPyodide();
-
+        window.pyodide = loadPyodide({ stdout: updateStatus, stderr: updateStatus })
         // fetch python script 
         const sourcecode = await fetch('pixelate.py').then(response => response.text())
         // load required packages
         await pyodide.loadPackagesFromImports(sourcecode)
         // execute python script
         pyodide.runPython(sourcecode)
-        msg.classList.remove("processing")
+        status.classList.remove("processing")
         msg.innerHTML = 'Processing image..'
     }
     // if there was an error on any step, notify the user
-    catch {
-        msg.classList.remove("processing")
-        alert('Initialization error. Please update the page.')
-    }
+    catch (e){
+        status.classList.remove("processing")
+        alert('Initialization error. Please update the page.\n'+e.message)
+        console.log("Error", e.stack);
+        console.log("Error", e.name);
+        console.log("Error", e.message);
+}
 
     // handle image upload
     inputFile.onchange = () => {
@@ -32,7 +41,7 @@ async function main() {
         const reader = new FileReader();
 
         reader.onloadend = () => {
-            msg.classList.add("processing")
+            status.classList.add("processing")
 
             source.src = reader.result
             setTimeout(() => {
@@ -43,15 +52,15 @@ async function main() {
                 const p = parseInt(pixelSize.value)
                 // execute python function
                 try {
-                    result.src = pyodide.globals.get('set_img')(img, w, p).toJS()
-                    // msg.classList.remove("processing")
+                    result.src = pyodide.globals.get('set_img')(img, w, p).toJs()
+                    // status.classList.remove("processing")
                 } catch (e) {
                     alert(e.message)
                     console.log("Error", e.stack);
                     console.log("Error", e.name);
                     console.log("Error", e.message);
                 }
-                msg.classList.remove("processing")
+                status.classList.remove("processing")
             }, 0)
         }
 
@@ -59,14 +68,14 @@ async function main() {
     }
 }
 
-const update = () => {
+function update() {
     msg.classList.add("processing")
     setTimeout(() => {
         const w = parseInt(windowSize.value)
         const p = parseInt(pixelSize.value)
         try {
             // execute python function
-            result.src = pyodide.globals.get('pixelate_dense')(w, p).toJS()
+            result.src = pyodide.globals.get('pixelate_dense')(w, p).toJs()
         } catch { alert('Python error') }
 
         msg.classList.remove("processing")
